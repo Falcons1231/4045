6ef67fdd79004e8ed32c06bbe5af30d3849911b7
Administrator <Administrator@DESKTOP-099UQRG>
2023-02-11 07:55:02 -0500

    Initial Commit

---
+++ src/test/java/com/hello/individualAssignment/IndividualAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.individualAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
